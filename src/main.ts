60a494324c37e38915e9e63864d67d96a882ad9a
monari <jobmonari7@gmail.com>
2025-07-28 18:10:36 +0300

    feat: changing ports

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, "0.0.0.0");
 }
 bootstrap();
